39ab72559338f6212d594b9a2adaede36d58ea13
Kirkland Poole <info@kirklandpoole.com>
2018-10-10 12:39:11 -0400

    Add Project files

---
+++ IoT-Alexa-Skill/src/facts.js
@@ -0,0 +1,18 @@
+// TODO Add more or replace entirely the facts below for your chosen history domain
+// Include at least 10 distinct facts with a 4-digit year included in the text for each
+module.exports = {
+    "FACTS_EN": [
+      "The field of AI is considered to have its origin in 1950, with publication of British mathematician Alan Turing's paper, Computing Machinery and Intelligence.",
+      "The term, Artificial Intelligence, was coined in 1956 by mathematician and computer scientist John McCarthy, at Dartmouth College, in New Hampshire.",
+      "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
+      "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
+      "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
+      "The field of AI research was born at a workshop at Dartmouth College in 1956.",
+      "In 1974, in response to the criticism of Sir James Lighthill and ongoing pressure from the US Congress to fund more productive projects, both the U.S. and British governments cut off exploratory research in AI. The next few years would later be called an AI winter a period when funding AI projects was difficult.",
+      "In the early 1980s, AI research was revived by the commercial success of expert systems, a form of AI program that simulated the knowledge and analytical skills of human experts.",
+      "By 1985 the market for AI had reached over a billion dollars. At the same time, Japans fifth generation computer project inspired the U.S and British governments to restore funding for academic research.",
+      "Beginning with the collapse of the Lisp Machine market in 1987, AI once again fell into disrepute, and a second, longer-lasting hiatus began.",
+      "In the late 1990s and early 21st century, AI began to be used for logistics, data mining, medical diagnosis and other areas.",
+      "In 1997, Deep Blue became the first computer chess-playing system to beat a reigning world chess champion."
+    ]
+}
